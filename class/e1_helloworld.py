6f8797fc030605db8698e485d9c08b9f5dc6a714
Lucha06 <lucerosanchez0341@gmail.com>
2024-03-04 12:11:51 -0600

    ex0 and ex1 done

--- class/e1_helloworld.py
+++ class/e1_helloworld.py
@@ -1,20 +1,20 @@
 # Instructions: print out the following sentences, the sentence must show as the comments
 
 # "Once Charles Dickens said: "It was the best of times, it was the worst of times""
-
+print('Once Charles Dickens said: "It was the best of times, it was the worst of times"')
 
 
 # 5    7    9    11
-
-
+print(5, 7, 9, 11, sep='    ')
+print(5, 7, 9, 11, sep='\t')
 """
 Actuarial
 Science
 """
-
+print('Actuarial', 'Science', sep='\n')
 
 # With just three '-' print out five '-'
-
+print('-', '-', '-', sep='-')
 
 # Hello world (this is the first program that all programmers do)
-
+print('Hello world')
