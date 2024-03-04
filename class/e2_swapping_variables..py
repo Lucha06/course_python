058258762940b75bfb574ea722252cdb7ce40546
Lucha06 <lucerosanchez0341@gmail.com>
2024-03-04 12:57:05 -0600

    Exercise 2 done

---
+++ class/e2_swapping_variables..py
@@ -0,0 +1,13 @@
+# 04-Marzo-2024
+# Definimos el valor de las variables
+a = 1
+b = 2
+
+# Comenzamos con las variables para intercambiar los valores
+c = a
+a = b
+b = c
+
+# Imprimimos las valores de las variables
+print('Valor de a', a, sep='\n')
+print('Valor de b', b, sep='\n')
